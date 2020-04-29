93836a9951a73b25a9fbd5cd5680b46ae4020e75
Kelvin Brito <britok30@gmail.com>
2020-04-29 02:03:58 -0400

    initial css

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
-            column number one
-          </div>
-          <div className="col-6">
-            column number one
-          </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-6">column number one</div>
+                <div className="col-6">
+                    <span>
+                        <i class="fas fa-home"></i>
+                    </span>
+                </div>
+            </div>
         </div>
-      </div>
     );
 }
 
